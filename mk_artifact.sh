c84f34379f85c7b3be9e9ff197701d578a175898
段体华 <duantihua@gmail.com>
2020-10-04 18:10:04 +0800

    support artifact sha1sum

--- mk_artifact.sh
+++ mk_artifact.sh
@@ -8,9 +8,11 @@ mk_artifact(){
   targetName=`grep "targetName " -R dub.sdl |awk 'NR==1{gsub(/"/,"");print $2}'`
   rm -rf target/$targetName-$version-$2.bin
   mv target/$targetName target/$targetName-$version-$2.bin
+  sha1sum target/$targetName-$version-$2.bin|awk 'NR==1{gsub(/"/,"");print $1}'>> target/$targetName-$version-$2.bin.sha1
   mkdir -p ~/.m2/repository/org/beangle/micdn/$targetName/$version/
   rm -rf ~/.m2/repository/org/beangle/micdn/$targetName/$version/$targetName-$version-$2.bin
   cp target/$targetName-$version-$2.bin ~/.m2/repository/org/beangle/micdn/$targetName/$version/$targetName-$version-$2.bin
+  cp target/$targetName-$version-$2.bin.sha1 ~/.m2/repository/org/beangle/micdn/$targetName/$version/$targetName-$version-$2.bin.sha1
   cd ..
 }
 
@@ -19,10 +21,10 @@ mk_artifact "asset" "ldc"
 mk_artifact "blob" "ldc"
 mk_artifact "maven" "ldc"
 
-dub build --build=release --compiler=dmd
-mk_artifact "asset" "dmd"
-mk_artifact "blob" "dmd"
-mk_artifact "maven" "dmd"
+# dub build --build=release --compiler=dmd
+# mk_artifact "asset" "dmd"
+# mk_artifact "blob" "dmd"
+# mk_artifact "maven" "dmd"
 
 cd $MICDN_HOME
 mkdir -p target
@@ -30,10 +32,10 @@ rm -rf target/beangle-micdn-$version.zip
 
 cd ~/.m2/repository
 zip  $MICDN_HOME/target/beangle-micdn-$version.zip org/beangle/micdn/beangle-micdn-asset/$version/beangle-micdn-asset-$version-ldc.bin \
+org/beangle/micdn/beangle-micdn-asset/$version/beangle-micdn-asset-$version-ldc.bin.sha1 \
 org/beangle/micdn/beangle-micdn-blob/$version/beangle-micdn-blob-$version-ldc.bin \
+org/beangle/micdn/beangle-micdn-blob/$version/beangle-micdn-blob-$version-ldc.bin.sha1 \
 org/beangle/micdn/beangle-micdn-maven/$version/beangle-micdn-maven-$version-ldc.bin \
-org/beangle/micdn/beangle-micdn-asset/$version/beangle-micdn-asset-$version-dmd.bin \
-org/beangle/micdn/beangle-micdn-blob/$version/beangle-micdn-blob-$version-dmd.bin \
-org/beangle/micdn/beangle-micdn-maven/$version/beangle-micdn-maven-$version-dmd.bin
+org/beangle/micdn/beangle-micdn-maven/$version/beangle-micdn-maven-$version-ldc.bin.sha1
 
 gpg -ab $MICDN_HOME/target/beangle-micdn-$version.zip
